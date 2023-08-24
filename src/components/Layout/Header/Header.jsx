c6ff6418f51cc30ba388c0511f02cd0392fb045a
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-08-24 17:47:09 +0300

    add logo div

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,20 +1,27 @@
 import { NavLink } from "react-router-dom";
-import css from "./temp-div-styles.module.scss";
+import cssTest from "./temp-div-styles.module.scss";
+import css from "./Header.module.scss"
+import Logo from "./Logo/Logo";
 
 export default function Header() {
 	return (
-		<header>
-			<p>Header</p>
-			<div className={css.tempNav}>
-				{/* Тимчасовий дів - створений для переходу між сторінками */}
-				<NavLink to="/welcome">welcome</NavLink>
-				<NavLink to="/">Main</NavLink>
-				<NavLink to="/drinks/1">Category 1</NavLink>
-				<NavLink to="/add">Add</NavLink>
-				<NavLink to="/favorite">Favorite</NavLink>
-				<NavLink to="/recipe/1">Recipe 1</NavLink>
-				<NavLink to="/error">Not found</NavLink>
-			</div>
+		<>
+		<header className={css.header}>
+			<Logo />
+
 		</header>
+
+		<p>Header</p>
+		<div className={cssTest.tempNav}>
+			{/* Тимчасовий дів - створений для переходу між сторінками */}
+			<NavLink to="/welcome">welcome</NavLink>
+			<NavLink to="/">Main</NavLink>
+			<NavLink to="/drinks/1">Category 1</NavLink>
+			<NavLink to="/add">Add</NavLink>
+			<NavLink to="/favorite">Favorite</NavLink>
+			<NavLink to="/recipe/1">Recipe 1</NavLink>
+			<NavLink to="/error">Not found</NavLink>
+		</div>
+</>
 	);
 }
