b8e72281b95f36314c32dab8eb74d5d0c0f0bebc
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-08-24 21:56:50 +0300

    welcome Page

--- src/components/Auth/SignupForm/SignupForm.jsx
+++ src/components/Auth/SignupForm/SignupForm.jsx
@@ -1,3 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Formik, Field, Form } from "formik";
+
 export default function SignupForm() {
-	return <div>SignupForm</div>;
+	return (
+		<div className="App">
+			<h1>Contact Us</h1>
+			<Formik
+			// initialValues={{ name: "", email: "" password:'' }}
+			// onSubmit={async values => {
+			// 	await new Promise(resolve => setTimeout(resolve, 500));
+			// 	alert(JSON.stringify(values, null, 2));
+			// }}
+			>
+				<Form>
+					<Field
+						name="name"
+						type="text"
+					/>
+					<Field
+						name="email"
+						type="email"
+					/>
+					<Field
+						name="password"
+						type="password"
+					/>
+					<button type="submit">Submit</button>
+				</Form>
+			</Formik>
+		</div>
+	);
 }
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,9 +1,30 @@
 import { Outlet } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function WelcomePage() {
 	return (
 		<>
-			<div>WelcomePage</div>
+			<div>
+				<h2 className="title_start">Welcome to the app!</h2>
+				<p className="text_start ">
+					This app offers more than just a collection of recipes - it is designed to be your very own digital cookbook. You can easily save and
+					retrieve your own recipes at any time.
+				</p>
+				<div className="list_start">
+					<NavLink
+						to="signup"
+						className="btn_start"
+					>
+						Registration
+					</NavLink>
+					<NavLink
+						to="signin"
+						className="btn_start"
+					>
+						Sign In
+					</NavLink>
+				</div>
+			</div>
 			<Outlet />
 		</>
 	);
