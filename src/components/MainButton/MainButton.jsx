a4c217ea05b59eb9e0493a2516026fe2db2ecf01
ansachuk <ansachuk@icloud.com>
2023-08-24 18:45:16 +0300

    add more vars and default button

--- src/components/MainButton/MainButton.jsx
+++ src/components/MainButton/MainButton.jsx
@@ -1,3 +1,15 @@
-export default function MainButton() {
-	return <div>MainButton</div>;
+import PropTypes from "prop-types";
+import css from "./MainButton.module.scss";
+
+export default function MainButton({ title, disabled = false }) {
+	return (
+		<button disabled={disabled} className={css.mainButton}>
+			{title}
+		</button>
+	);
 }
+
+MainButton.propTypes = {
+	title: PropTypes.string.isRequired,
+	disabled: PropTypes.bool,
+};
