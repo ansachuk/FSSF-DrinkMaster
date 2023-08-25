e55a4d2b7f520b89bf3bbfa2b9048de16939b153
olena-nikitchenko <helnikitka@gmail.com>
2023-08-26 02:27:43 +0300

    added styles to delete and see button

--- src/components/DeleteButton/DeleteButton.jsx
+++ src/components/DeleteButton/DeleteButton.jsx
@@ -1,3 +1,39 @@
-export default function DeleteButton() {
-	return <div>DeleteButton</div>;
+import React from "react";
+// import { useDispatch } from "react-redux";
+// import { removeFromFavoriteThunk, removeRecipeThunk } from "redux/Cocktails/cocktailsOperations";
+import css from "./DeleteButton.module.scss";
+
+const trashPath = new URL("../../images/icons.svg", import.meta.url);
+
+export default function DeleteButton({ id, page }) {
+	// const dispatch = useDispatch();
+	// const onDelete = () => {
+	// 	if (page === "favorite") {
+	// 		dispatch(removeFromFavoriteThunk(id));
+
+	// 		return;
+	// 	}
+	// 	if (page === "my") {
+	// 		dispatch(removeRecipeThunk(id));
+
+	return (
+		<>
+			<button
+				className={css.deleteButton}
+				// onClick={onDelete}
+				data-page={page}
+				data-id={id}
+			>
+				<svg
+					className={css.iconTrash}
+					width="50"
+					height="50"
+				>
+					<use href={trashPath + "#remove"}></use>
+				</svg>
+
+				{/* <span className={css.iconTrash}></span> */}
+			</button>
+		</>
+	);
 }
