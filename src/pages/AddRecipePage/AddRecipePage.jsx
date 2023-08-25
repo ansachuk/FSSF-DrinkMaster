e21dc5b070339ca8316f57bd5e5bdb4cb584b35d
Yevhenii2022 <3starosta3@gmail.com>
2023-08-25 12:56:18 +0300

    add

--- src/pages/AddRecipePage/AddRecipePage.jsx
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -4,18 +4,47 @@ import AddRecipeForm from "../../components/AddRecipe/AddRecipeForm/AddRecipeFor
 import FollowUs from "../../components/AddRecipe/FollowUs/FollowUs";
 import PopularRecipe from "../../components/AddRecipe/PopularRecipe/PopularRecipe";
 import MainPageTitle from "../../components/MainPageTitle/MainPageTitle";
+import icons from "../../images/icons.svg";
 
 export default function AddRecipePage() {
 	return (
 		<div>
 			<MainPageTitle />
+			<div className={css.imageContainer}>
+				<div>
+					<div>
+						<button className={css.btnContainer}>
+							<svg
+								className={css.btnIcon}
+								width="30"
+								height="30"
+							>
+								<use href={icons + "#plus"}></use>
+							</svg>
+						</button>
+					</div>
+					<p>Add image</p>
+				</div>
+			</div>
 			<AddRecipeForm />
 			<FollowUs />
 			<PopularRecipe />
 		</div>
 	);
 }
-
+// <button
+// 	class="mobile__logout"
+// 	type="submit"
+// >
+// 	Log out
+// 	<svg
+// 		class="logout__icon"
+// 		width="20"
+// 		height="20"
+// 	>
+// 		<use href="./images/icons.svg#arrow-narrow-right"></use>
+// 	</svg>
+// </button>
 AddRecipePage.propTypes = {
 	// title: PropTypes.string.isRequired,
 	propClass: PropTypes.string,
