80f8d011c4f40aaa7c7d52c81b6d8cdaebfe0b37
Yevhenii2022 <3starosta3@gmail.com>
2023-08-25 17:39:13 +0300

    add

--- src/components/AddRecipe/RecipeDescriptionFields/RecipeDescriptionFields.jsx
+++ src/components/AddRecipe/RecipeDescriptionFields/RecipeDescriptionFields.jsx
@@ -1,7 +1,28 @@
 import PropTypes from "prop-types";
 import css from "./RecipeDescriptionFields.module.scss";
+import icons from "../../../images/icons.svg";
 export default function RecipeDescriptionFields() {
-	return <div>RecipeDescriptionFields</div>;
+	return (
+		<div className={css.imageContainer}>
+			<div>
+				<button
+					className={css.btnContainer}
+					type="button"
+				>
+					<svg
+						className={css.btnIcon}
+						width="28"
+						height="28"
+					>
+						<use href={icons + "#plus"}></use>
+					</svg>
+				</button>
+			</div>
+			<div>
+				<p className={css.btnText}>Add image</p>
+			</div>
+		</div>
+	);
 }
 
 RecipeDescriptionFields.propTypes = {
--- src/pages/AddRecipePage/AddRecipePage.jsx
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -1,50 +1,21 @@
 import PropTypes from "prop-types";
-import css from "./AddRecipePage.module.scss";
+// import css from "./AddRecipePage.module.scss";
 import AddRecipeForm from "../../components/AddRecipe/AddRecipeForm/AddRecipeForm";
 import FollowUs from "../../components/AddRecipe/FollowUs/FollowUs";
 import PopularRecipe from "../../components/AddRecipe/PopularRecipe/PopularRecipe";
 import MainPageTitle from "../../components/MainPageTitle/MainPageTitle";
-import icons from "../../images/icons.svg";
 
 export default function AddRecipePage() {
 	return (
-		<div>
+		<>
 			<MainPageTitle />
-			<div className={css.imageContainer}>
-				<div>
-					<div>
-						<button className={css.btnContainer}>
-							<svg
-								className={css.btnIcon}
-								width="30"
-								height="30"
-							>
-								<use href={icons + "#plus"}></use>
-							</svg>
-						</button>
-					</div>
-					<p>Add image</p>
-				</div>
-			</div>
 			<AddRecipeForm />
 			<FollowUs />
 			<PopularRecipe />
-		</div>
+		</>
 	);
 }
-// <button
-// 	class="mobile__logout"
-// 	type="submit"
-// >
-// 	Log out
-// 	<svg
-// 		class="logout__icon"
-// 		width="20"
-// 		height="20"
-// 	>
-// 		<use href="./images/icons.svg#arrow-narrow-right"></use>
-// 	</svg>
-// </button>
+
 AddRecipePage.propTypes = {
 	// title: PropTypes.string.isRequired,
 	propClass: PropTypes.string,
