140f8345a88a7c8fbd13752bbc53f32d90e535b9
Heorhii <gerythepatient@gmail.com>
2023-08-25 18:27:25 +0200

    Add styles to RecipePageHero

--- src/pages/RecipePage/RecipePageHero/RecipePageHero.jsx
+++ src/pages/RecipePage/RecipePageHero/RecipePageHero.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { favoriteRecipeToggle } from 'redux/operations';
 import { MainButton } from '../../../components/MainButton/MainButton';
+import css from './RecipePageHero.module.scss';
+import stylesForButton from '../../../components/MainButton/MainButton.module.scss';
 
 const RecipePageHero = ({glass, title, about, image}) => {
     const dispatch = useDispatch();
@@ -11,13 +13,13 @@ const RecipePageHero = ({glass, title, about, image}) => {
     const isFavorite = favoriteRecipes.includes(recipeId);
 
     return (
-        <>
-            <p>{glass}</p>
-            <Title>{title}</Title>
-            <p>{about}</p>
-            <MainButton onClick={() => dispatch(favoriteRecipeToggle(recipeId))}>{isFavorite ? 'Remove from favorite recipes' : 'Add to favorite recipes'}</MainButton>
-            <img src={image} alt={title} />
-        </>
+        <div className={css.recipePageHeroContainer}>
+            <p className={css.glassName}>{glass}</p>
+            <Title className={css.recipePageTitle}>{title}</Title>
+            <p className={css.aboutDrink}>{about}</p>
+            <MainButton onClick={() => dispatch(favoriteRecipeToggle(recipeId))} className={stylesForButton.recipePageButton}>{isFavorite ? 'Remove from favorite recipes' : 'Add to favorite recipes'}</MainButton>
+            <img src={image} alt={title} className={css.recipePageHeroImg} />
+        </div>
     )
 };
 
