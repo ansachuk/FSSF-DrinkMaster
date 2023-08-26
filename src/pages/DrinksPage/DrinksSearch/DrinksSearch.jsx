bf951d32bfe7ca944140a26a235253e92faa7c59
Tetiana Pesotska <verhulev81@gmail.com>
2023-08-26 20:46:14 +0300

    fixed

--- src/pages/DrinksPage/DrinksSearch/DrinksSearch.jsx
+++ src/pages/DrinksPage/DrinksSearch/DrinksSearch.jsx
@@ -1,78 +1,165 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setSearchText, setSelectedCategory, setSelectedIngredient, setIngredientsList } from "./actions";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 import { drinksCategoriesList } from "../../../data/drinksData";
+
+// action//
+export const setSearchText = text => ({
+	type: "SET_SEARCH_TEXT",
+	payload: text,
+});
+
+export const setSelectedCategory = category => ({
+	type: "SET_SELECTED_CATEGORY",
+	payload: category,
+});
+
+export const setSelectedIngredient = ingredient => ({
+	type: "SET_SELECTED_INGREDIENT",
+	payload: ingredient,
+});
+
+export const setIngredientsList = ingredients => ({
+	type: "SET_INGREDIENTS_LIST",
+	payload: ingredients,
+});
+
+// редюсер//
+const initialState = {
+	searchText: "",
+	selectedCategory: "All Categories",
+	selectedIngredient: "Ingredients",
+	ingredientsList: [],
+};
+
+const rootReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case "SET_SEARCH_TEXT":
+			return { ...state, searchText: action.payload };
+		case "SET_SELECTED_CATEGORY":
+			return { ...state, selectedCategory: action.payload };
+		case "SET_SELECTED_INGREDIENT":
+			return { ...state, selectedIngredient: action.payload };
+		case "SET_INGREDIENTS_LIST":
+			return { ...state, ingredientsList: action.payload };
+		default:
+			return state;
+	}
+};
+//  схема валидации//
+
+const validationSchema = Yup.object().shape({
+	searchText: Yup.string().required("Search text is required"),
+});
 
 export default function DrinksSearch() {
 	const dispatch = useDispatch();
 	const { searchText, selectedCategory, selectedIngredient, ingredientsList } = useSelector(state => state);
 
 	useEffect(() => {
-		//  запрос к бэкенду для получения списка ингредиентов
-
-		fetch("/api/ingredients")
-			.then(response => response.json())
-			.then(data => dispatch(setIngredientsList(data)));
+		// Запрос к бэкенду для получения списка ингредиентов
+		axios
+			.get("/api/ingredients")
+			.then(response => {
+				const data = response.data;
+				dispatch(setIngredientsList(data));
+			})
+			.catch(error => {
+				console.error("Error fetching ingredients:", error);
+			});
 	}, []);
 
-	const handleSearchInputChange = event => {
-		const inputText = event.target.value;
-		dispatch(setSearchText(inputText));
-
-		// запрос на получение рецептов
-		// с использованием выбранных параметров: inputText, selectedCategory, selectedIngredient
-		console.log("Выполняется поиск с параметрами:", {
-			inputText,
-			selectedCategory,
-			selectedIngredient,
-		});
-	};
-
-	const handleCategoryChange = event => {
-		dispatch(setSelectedCategory(event.target.value));
-	};
-
-	const handleIngredientChange = event => {
-		dispatch(setSelectedIngredient(event.target.value));
-	};
-
 	return (
-		<div>
-			<input
-				type="text"
-				placeholder="Enter the text"
-				value={searchText}
-				onChange={handleSearchInputChange}
-			/>
-			<select
-				value={selectedCategory}
-				onChange={handleCategoryChange}
-			>
-				<option value="All Categories">All Categories</option>
-				{drinksCategoriesList.map((category, index) => (
-					<option
-						key={index}
-						value={category}
+		<Formik
+			initialValues={{
+				searchText: "",
+				selectedCategory: "All Categories",
+				selectedIngredient: "Ingredients",
+			}}
+			validationSchema={validationSchema}
+			onSubmit={async values => {
+				try {
+					// Отправка данных на бекенд для примера
+					const response = await axios.post("/api/submit", values);
+
+					if (response.status === 200) {
+						// Обработка успешного ответа от сервера
+						console.log("Data successfully submitted");
+					} else {
+						// Обработка ошибки
+						console.error("Error submitting data");
+					}
+				} catch (error) {
+					console.error("Error submitting data:", error);
+				}
+			}}
+		>
+			<Form>
+				<div>
+					<Field
+						type="text"
+						placeholder="Enter the text"
+						name="searchText"
+					/>
+					<button type="submit">Х</button>
+					<ErrorMessage
+						name="searchText"
+						component="div"
+					/>
+				</div>
+				<div>
+					<Field
+						as="select"
+						name="selectedCategory"
 					>
-						{category}
-					</option>
-				))}
-			</select>
-			<select
-				value={selectedIngredient}
-				onChange={handleIngredientChange}
-			>
-				<option value="Ingredients">Ingredients</option>
-				{ingredientsList.map((ingredient, index) => (
-					<option
-						key={index}
-						value={ingredient.title}
-					>
-						{ingredient.title}
-					</option>
-				))}
-			</select>
-		</div>
+						<option value="All Categories">All Categories</option>
+						{drinksCategoriesList.map((category, index) => (
+							<option
+								key={index}
+								value={category}
+							>
+								{category}
+							</option>
+						))}
+					</Field>
+				</div>
+				<div>
+					<Field
+						name="selectedIngredient"
+						render={({ field }) => (
+							<div>
+								<select
+									{...field}
+									onChange={async event => {
+										const selectedId = event.target.value;
+										try {
+											const response = await axios.get(`/api/ingredients/${selectedId}`);
+											const ingredient = response.data;
+											// Обновление состояния с полученным ингредиентом
+											dispatch(setSelectedIngredient(ingredient));
+										} catch (error) {
+											console.error("Error fetching ingredient:", error);
+										}
+									}}
+								>
+									<option value="Ingredients">Ingredients</option>
+									{ingredientsList.map((ingredient, index) => (
+										<option
+											key={index}
+											value={ingredient.id}
+										>
+											{ingredient.title}
+										</option>
+									))}
+								</select>
+							</div>
+						)}
+					/>
+				</div>
+			</Form>
+		</Formik>
 	);
 }
