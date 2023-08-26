ed8939a92d02dd06e0c923bbff02e0728d8cc88d
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-08-26 19:57:00 +0300

    image class

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,40 +1,50 @@
 import { Outlet } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
-import { titleStart, textStart, listStart, btnStart, containerWelcome } from "./WelcomePage.module.scss";
-
+import { titleStart, textStart, listStart, btnStart, containerWelcome, image } from "./WelcomePage.module.scss";
+import Container from "../../components/Container/Container";
 export default function WelcomePage() {
 	return (
 		<>
-			<img
-				src="src/images/static/welcome/welcome.jpg"
-				alt="glass with coctails"
-				width="225"
-				height="225"
-				loading="lazy"
-			/>
-			<div className={containerWelcome}>
-				<h2 className={titleStart}>Welcome to the app!</h2>
-				<p className={textStart}>
-					This app offers more than just a collection of recipes - it is designed to be your very own digital cookbook. You can easily save and
-					retrieve your own recipes at any time.
-				</p>
-				<div className={listStart}>
-					<NavLink
-						to="/signup"
-						className={btnStart}
-					>
-						Registration
-					</NavLink>
-					<NavLink
-						to="/signin"
-						className={btnStart}
-					>
-						Sign In
-					</NavLink>
+			<div
+				style={{
+					position: "relative",
+					justifyContent: "center",
+					display: "flex",
+					padding: "20px 0",
+				}}
+			>
+				<img
+					className={image}
+					src="src/images/static/welcome/welcome.jpg"
+					alt="glass with coctails"
+					width="587"
+					height="812"
+					loading="lazy"
+				/>
+				<div className={containerWelcome}>
+					<h2 className={titleStart}>Welcome to the app!</h2>
+					<p className={textStart}>
+						This app offers more than just a collection of recipes - it is designed to be your very own digital cookbook. You can easily save and
+						retrieve your own recipes at any time.
+					</p>
+					<div className={listStart}>
+						<NavLink
+							to="/signup"
+							className={btnStart}
+						>
+							Registration
+						</NavLink>
+						<NavLink
+							to="/signin"
+							className={btnStart}
+						>
+							Sign In
+						</NavLink>
+					</div>
 				</div>
+				<Outlet />
 			</div>
-			<Outlet />
 		</>
 	);
 }
