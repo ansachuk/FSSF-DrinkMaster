53c3bfef327c3dc991230df494a7f17e2db0f1e5
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-08-28 18:15:06 +0300

    add image input

--- src/components/Layout/Header/UserMenu/EditProfileModal/EditProfileModal.jsx
+++ src/components/Layout/Header/UserMenu/EditProfileModal/EditProfileModal.jsx
@@ -3,8 +3,42 @@ import icons from "../../../../../images/icons.svg";
 import css from "./EditProfileModal.module.scss";
 import buttonCss from "../../../../MainButton/MainButton.module.scss";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 export default function EditProfileModal({ handlerEditProfileClick }) {
+	const [image, setImage] = useState({ preview: "", data: "" });
+
+	const userInfoFormSubmit = e => {
+		e.preventDefault();
+		console.dir(e.target);
+		const userName = e.target.elements.user_name.value;
+		console.log(userName);
+		// const userImage = e.target.elements.file_upload.files[0];
+		// console.log(userImage);
+
+		const formData = new FormData();
+		formData.append("file", image.data);
+		console.log(formData);
+		// 	const response = await fetch('http://localhost:5000/image', {
+		//   method: 'POST',
+		//   body: {formData, userName},
+		// })
+	};
+
+	const onImageChange = e => {
+		const img = {
+			preview: URL.createObjectURL(e.target.files[0]),
+			data: e.target.files[0],
+		};
+		console.log(img);
+		setImage(img);
+	};
+
+	useEffect(() => {
+		const userImage = document.getElementById("user_image");
+		userImage.src = image.preview;
+	}, [image.preview]);
+
 	return (
 		<div className={css.edit_container}>
 			<button
@@ -15,31 +49,47 @@ export default function EditProfileModal({ handlerEditProfileClick }) {
 					<use href={icons + "#close"}></use>
 				</svg>
 			</button>
-			<div className={css.icon_container}>
-				<svg className={css.user_large_icon}>
-					<use href={icons + "#user"}></use>
-				</svg>
-				<svg
-					className={css.plus_icon}
-					viewBox="-5 -5 70 100"
-				>
-					<use href={icons + "#plus"}></use>
-				</svg>
-			</div>
-			<label className={css.input_container}>
-				<input
-					type="text"
-					placeholder="User name"
-					className={css.input}
+			<form onSubmit={userInfoFormSubmit}>
+				<div className={css.icon_container}>
+					<img
+						src={icons + "#user"}
+						alt="User photo"
+						id="user_image"
+						className={css.user_large_icon}
+					/>
+					{/* <svg className={css.user_large_icon}>
+						<use href={icons + "#user"}></use>
+					</svg> */}
+					<input
+						id="file_upload"
+						type="file"
+						onChange={onImageChange}
+					/>
+					<label htmlFor="file_upload">
+						<svg
+							className={css.plus_icon}
+							viewBox="-5 -5 70 100"
+						>
+							<use href={icons + "#plus"}></use>
+						</svg>
+					</label>
+				</div>
+				<label className={css.input_container}>
+					<input
+						id="user_name"
+						type="text"
+						placeholder="User name"
+						className={css.input}
+					/>
+					<svg className={css.input_pen_icon}>
+						<use href={icons + "#pen"}></use>
+					</svg>
+				</label>
+				<MainButton
+					propClass={buttonCss.largeButton}
+					title="Save changes"
 				/>
-				<svg className={css.input_pen_icon}>
-					<use href={icons + "#pen"}></use>
-				</svg>
-			</label>
-			<MainButton
-				propClass={buttonCss.largeButton}
-				title="Save changes"
-			/>
+			</form>
 		</div>
 	);
 }
