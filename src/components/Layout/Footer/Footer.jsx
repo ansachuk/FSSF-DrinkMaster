d9318127f7981a7d693814f91bdfbbe1e3ca5222
ansachuk <ansachuk@icloud.com>
2023-08-28 22:20:16 +0300

    fix redux bugs

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,15 +1,3 @@
-import { useDispatch } from "react-redux";
-import MainButton from "../../MainButton/MainButton";
-import { byID } from "../../../redux/operations/recipiesOperations";
-
 export default function Footer() {
-	const disp = useDispatch();
-	return (
-		<div>
-			<MainButton
-				onClick={() => disp(byID("639b6de9ff77d221f190c50f"))}
-				title="by id"
-			/>
-		</div>
-	);
+	return <div>Footer</div>;
 }
