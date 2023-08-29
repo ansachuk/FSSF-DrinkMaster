f4eb7d43b0d4c437d8fb8ec26a6a90057aa1cf1c
ansachuk <ansachuk@icloud.com>
2023-08-29 19:08:01 +0300

    fix useEffect

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -4,10 +4,10 @@
 
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { byID } from '../../redux/operations/recipiesOperations.js';
+import { useDispatch, useSelector } from "react-redux";
+import { byID } from "../../redux/operations/recipiesOperations.js";
 import { selectByID } from "../../redux/selectors/recipieSelectors.js";
-import MainTitle from '../../components/MainTitle/MainTitle.jsx';
+import MainTitle from "../../components/MainTitle/MainTitle.jsx";
 import RecipePageHero from "./RecipePageHero/RecipePageHero";
 import RecipeIngredientsList from "./RecipeIngredientsList/RecipeIngredientsList";
 import RecipePreparation from "./RecipePreparation/RecipePreparation";
@@ -17,16 +17,23 @@ const RecipePage = () => {
 	const { recipeId } = useParams();
 	const recipe = useSelector(selectByID);
 
-useEffect(() => {
-dispatch(byID(recipeId));
-}, []);
-	
-	return (recipe.glass && <MainTitle title={recipe.drink}>
-		<RecipePageHero glass={recipe.glass} about={recipe.about || null} image={recipe.drinkThumb || null}></RecipePageHero>
-		<RecipeIngredientsList ingredients={recipe.ingredients}></RecipeIngredientsList>
-		<RecipePreparation instructions={recipe.instructions}></RecipePreparation>
-		</MainTitle>
-	)
-	};
+	useEffect(() => {
+		dispatch(byID(recipeId));
+	}, [dispatch, recipeId]);
+
+	return (
+		recipe.glass && (
+			<MainTitle title={recipe.drink}>
+				<RecipePageHero
+					glass={recipe.glass}
+					about={recipe.about || null}
+					image={recipe.drinkThumb || null}
+				></RecipePageHero>
+				<RecipeIngredientsList ingredients={recipe.ingredients}></RecipeIngredientsList>
+				<RecipePreparation instructions={recipe.instructions}></RecipePreparation>
+			</MainTitle>
+		)
+	);
+};
 
 export default RecipePage;
