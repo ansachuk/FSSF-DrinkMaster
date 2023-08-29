b44763290deaf521dd52df55146a0e773be74cfc
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2023-08-29 20:55:34 +0300

    feature/MainPage-1st-commit

---
+++ src/pages/MainPage/DrinksList/DrinksList.jsx
@@ -0,0 +1,21 @@
+// import DrinkCard from "../DrinkCard/DrinkCard";
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { selectCocktails } from "../../../redux/selectors";
+
+// const DrinksList = () => {
+// 	const cocktails = useSelector(selectCocktails);
+// 	return (
+// 		<ul>
+// 			{cocktails.length > 1 &&
+// 				cocktails.map(cocktail => (
+// 					<DrinkCard
+// 						key={cocktail._id}
+// 						cocktail={cocktail}
+// 					/>
+// 				))}
+// 		</ul>
+// 	);
+// };
+
+// export default DrinksList;
