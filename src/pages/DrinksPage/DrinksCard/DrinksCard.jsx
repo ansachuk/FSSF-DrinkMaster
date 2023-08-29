f33567b5e5b6eb2cf0f9d68a7f605db69cd80b5d
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-08-29 23:03:28 +0300

    add DrinkList

--- src/pages/DrinksPage/DrinksCard/DrinksCard.jsx
+++ src/pages/DrinksPage/DrinksCard/DrinksCard.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { SeeButton, DeleteButton } from 'components';
 import drink from '../../../images/static/drinks/DrinkDefault.jpg';
 import css from './DrinkCard.module.scss';
 
-export const DrinkCard = ({ cocktail, page }) => {
+export const DrinkCard = ({ cocktail }) => {
   return (
     <div className={css.card}>
       <Link to={`/main/recipe/${cocktail._id}`}>
@@ -22,24 +21,14 @@ export const DrinkCard = ({ cocktail, page }) => {
         </div>
       </Link>
       <div className={css.wrapper}>
-        <div className={`${css.ingredientsWrapper} ${page === 'my' || page === 'favorite' ? css.my : ''}`}>
+        <div className={css.ingredientsWrapper}>
           <Link to={`/main/recipe/${cocktail._id}`}>
             <p className={css.drinkingreds}>{cocktail.drink}</p>
           </Link>
           <span className={css.ingredients}>Ingredients</span>
         </div>
       </div>
-      {(page === 'my' || page === 'favorite') && (
-        <p className={css.aboutStyled}>{cocktail.about}</p>
-      )}
-      <div className={css.buttonsWrapper}>
-        {(page === 'my' || page === 'favorite') && (
-          <SeeButton id={cocktail._id} />
-        )}
-        {(page === 'my' || page === 'favorite') && (
-          <DeleteButton id={cocktail._id} page={page} />
-        )}
-      </div>
+      <p className={css.aboutStyled}>{cocktail.about}</p>
     </div>
   );
 };
