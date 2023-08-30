de73a5d93d8ef0214770140661aab5ed1d492e29
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-08-30 12:07:37 +0300

    debug active nav link styles

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -11,6 +11,7 @@ export default function Header() {
 	const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1280);
 
 	const updateMedia = () => {
+		console.log(isDesktop);
 		setIsDesktop(window.innerWidth >= 1280);
 	};
 
