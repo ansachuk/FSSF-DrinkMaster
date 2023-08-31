07e1cea60cbc6e2b3ca4422ab1352f9064b8f100
olena-nikitchenko <helnikitka@gmail.com>
2023-09-01 00:08:50 +0300

    added change

--- src/components/RecipesItem/RecipesItem.jsx
+++ src/components/RecipesItem/RecipesItem.jsx
@@ -5,7 +5,7 @@ import SeeButton from "../../components/SeeButton/SeeButton";
 import imgDefault from "../../images/static/my-recepies/imgdefault.jpg";
 import css from "./RecipesItem.module.scss";
 
-export default function RecipesItem({ _id, drink, drinkThumb, about, onDeleteClick, page }) {
+export default function RecipesItem({ _id, drink, drinkThumb, about, instructions, page }) {
 	return (
 		<div className={css.card}>
 			<Link to={`/recipe/${_id}`}>
@@ -33,18 +33,15 @@ export default function RecipesItem({ _id, drink, drinkThumb, about, onDeleteCli
 					<p className={css.ingredientsText}>Ingredients</p>
 				</div>
 			</div>
-			<p className={css.about}>{about}</p>
+			{page === "favorite" && <p className={css.about}>{instructions}</p>}
+			{page === "my" && <p className={css.about}>{about}</p>}
 			<div className={css.buttonsWrapper}>
 				{(page === "my" || page === "favorite") && <SeeButton id={_id} />}
 				{(page === "my" || page === "favorite") && (
 					<DeleteButton
 						key={_id}
 						id={_id}
-						drink={drink}
-						drinkThumb={drinkThumb}
-						about={about}
 						page={page}
-						onDeleteClick={() => onDeleteClick(_id, "favorite")}
 					/>
 				)}
 			</div>
