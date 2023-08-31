4a318ee8ce70be04ac7a6c2c176d99d661a09f99
Kirill <94064326+Fantom19@users.noreply.github.com>
2023-09-01 00:36:36 +0300

    fix bug server part

--- src/components/Layout/Footer/EmailSubscription/EmailSubscription.jsx
+++ src/components/Layout/Footer/EmailSubscription/EmailSubscription.jsx
@@ -5,44 +5,39 @@ import { subscribe } from "../../../../redux/operations/authOperations";
 import scss from "./EmailSubscription.module.scss";
 
 const SubscribeForm = () => {
-	// Получаем метод dispatch из redux
 	const dispatch = useDispatch();
-	// Локальные состояния
-	const [isConfirmed, setIsConfirmed] = useState(false); // Для отслеживания подтверждения подписки
-	const [isFocused, setIsFocused] = useState(false); // Для отслеживания фокуса на поле ввода
 
-	// Обработчики фокуса и размытия
+	const [isConfirmed, setIsConfirmed] = useState(false);
+	const [isFocused, setIsFocused] = useState(false);
+
 	const handleFocus = () => {
-		setIsFocused(true); // Поле в фокусе
+		setIsFocused(true);
 	};
 
 	const handleBlur = () => {
-		setIsFocused(false); // Поле не в фокусе
+		setIsFocused(false);
 	};
 
-	// Обработчик отправки формы
 	const handleSubmit = async (values, { setSubmitting, setStatus }) => {
 		try {
-			// Отправляем запрос на подписку и ждем завершения
 			await dispatch(subscribe(values));
-			setIsConfirmed(true); // Подписка успешно подтверждена
-			setStatus("success"); // Устанавливаем статус успеха
+			setIsConfirmed(true);
+			setStatus("success");
 		} catch (error) {
-			setStatus("error"); // Устанавливаем статус ошибки
+			setStatus("error");
 		} finally {
-			setSubmitting(false); // Завершаем процесс отправки формы
+			setSubmitting(false);
 		}
 	};
 
-	// Эффект для очистки подтверждения подписки
 	useEffect(() => {
 		if (isConfirmed) {
 			const timer = setTimeout(() => {
-				setIsConfirmed(false); // Сбрасываем подтверждение подписки
+				setIsConfirmed(false);
 			}, 3000);
 
 			return () => {
-				clearTimeout(timer); // Очищаем таймер при размонтировании
+				clearTimeout(timer);
 			};
 		}
 	}, [isConfirmed]);
@@ -55,14 +50,14 @@ const SubscribeForm = () => {
 				const errors = {};
 
 				if (!values.email) {
-					return errors; // Возвращаем пустой объект ошибок, если поле пустое
+					return errors;
 				}
 
 				if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-					errors.email = "Invalid email address."; // Устанавливаем сообщение об ошибке для некорректного email
+					errors.email = "Invalid email address.";
 				}
 
-				return errors; // Возвращаем объект с ошибками (или пустой, если ошибок нет)
+				return errors;
 			}}
 		>
 			{({ isSubmitting, status, errors, values }) => (
@@ -75,7 +70,6 @@ const SubscribeForm = () => {
 							type="email"
 							name="email"
 							placeholder="Enter your email"
-							// Применяем классы в зависимости от фокуса, наличия значения и ошибок
 							className={`${scss.subscribe_input} ${
 								isFocused
 									? values.email
@@ -88,14 +82,13 @@ const SubscribeForm = () => {
 							onFocus={handleFocus}
 							onBlur={handleBlur}
 						/>
-						{/* Показываем сообщение об ошибке, если есть ошибка и поле в фокусе */}
+
 						{errors.email && isFocused && (
 							<div className={scss.error_message_on_border}>{errors.email}</div>
 						)}
 					</div>
 
 					<div className={scss.subscribe_status_container}>
-						{/* Отображаем сообщение об успехе, если статус "success" и подписка подтверждена */}
 						{status === "success" && isConfirmed && (
 							<p
 								className={`${scss.success_message} ${scss.status_message} ${scss.success_status}`}
@@ -104,17 +97,15 @@ const SubscribeForm = () => {
 							</p>
 						)}
 
-						{/* Отображаем сообщение об ошибке, если статус "error" */}
 						{status === "error" && (
 							<p className={`${scss.error_message} ${scss.status_message} ${scss.error_status}`}>
 								Oops! An error occurred. Please try again later.
 							</p>
 						)}
 					</div>
-					{/* Кнопка отправки формы */}
+
 					<button
 						type="submit"
-						// Отключаем кнопку, если отправка уже идет, поле пустое или есть ошибки
 						disabled={isSubmitting || !values.email || Object.keys(errors).length > 0}
 						className={scss.subscribe_button}
 					>
