feec9e81f594f55c588f3d5d5c9c66de70c2d8bf
olena-nikitchenko <helnikitka@gmail.com>
2023-08-31 14:36:24 +0300

    change code

--- src/components/RecipesItem/RecipesItem.jsx
+++ src/components/RecipesItem/RecipesItem.jsx
@@ -1,27 +1,35 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import DeleteButton from "../../components/DeleteButton/DeleteButton";
 import SeeButton from "../../components/SeeButton/SeeButton";
-import css from "./RecipesItem.module.scss";
 import imgDefault from "../../images/static/my-recepies/imgdefault.jpg";
+import css from "./RecipesItem.module.scss";
 
 export default function RecipesItem({ _id, drink, drinkThumb, about, onDeleteClick, page }) {
 	return (
 		<div className={css.card}>
-			<div className={css.imageBox}>
-				<div className={css.imageWrapper}>
-					<img
-						className={css.image}
-						src={drinkThumb}
-						alt={drink}
-						onError={e => {
-							e.currentTarget.src = imgDefault;
-						}}
-					/>
+			<Link to={`/recipe/${_id}`}>
+				<div className={css.imageBox}>
+					<div className={css.imageWrapper}>
+						<img
+							className={css.image}
+							src={drinkThumb}
+							alt={drink}
+							onError={e => {
+								e.currentTarget.src = imgDefault;
+							}}
+						/>
+					</div>
 				</div>
-			</div>
+			</Link>
 			<div>
-				<div className={css.ingredientsWrapper}>
-					<p className={css.ingredientsTitle}>{drink}</p>
+				<div
+					position={page}
+					className={css.ingredientsWrapper}
+				>
+					<Link to={`/recipe/${_id}`}>
+						<p className={css.ingredientsTitle}>{drink}</p>
+					</Link>
 					<p className={css.ingredientsText}>Ingredients</p>
 				</div>
 			</div>
