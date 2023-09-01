45dc768e8843a94fab137608fef195362bebe186
olena-nikitchenko <helnikitka@gmail.com>
2023-09-01 11:18:28 +0300

    added myRecipesPage

--- src/pages/MyRecipesPage/MyRecipesPage.jsx
+++ src/pages/MyRecipesPage/MyRecipesPage.jsx
@@ -1,3 +1,41 @@
+// export default function MyRecipesPage() {
+// 	return <div>MyRecipesPage</div>;
+// }
+
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectPage } from "../../redux/selectors/recipieSelectors.js";
+import MainTitle from "../../components/MainTitle/MainTitle";
+import RecipesList from "../../components/RecipesList/RecipesList";
+// import Paginator from "../../components/Paginator/Paginator";
+import { add } from "../../redux/operations/recipiesOperations.js";
+import css from "./MyRecipesPage.module.scss";
+
+const desktopLimit = 9;
+const tabletLimit = 8;
+
 export default function MyRecipesPage() {
-	return <div>MyRecipesPage</div>;
+	const dispatch = useDispatch();
+	const page = useSelector(selectPage);
+	const isDesktop = typeof window !== "undefined" && window.innerWidth >= 1440;
+	const limit = isDesktop ? desktopLimit : tabletLimit;
+
+	useEffect(() => {
+		dispatch(add({ page, limit }));
+	}, [page, limit, dispatch]);
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	return (
+		<>
+			<div className={css.section}>
+				<MainTitle title="My recipes">
+					<RecipesList />
+					{/* <Paginator /> */}
+				</MainTitle>
+			</div>
+		</>
+	);
 }
