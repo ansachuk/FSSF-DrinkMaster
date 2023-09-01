eb93e4932fba967821edcea1df7e4ec2c05731db
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2023-09-01 13:28:56 +0300

    feature/MainPage-mod

--- src/pages/MainPage/DrinkCard/DrinkCard.jsx
+++ src/pages/MainPage/DrinkCard/DrinkCard.jsx
@@ -7,27 +7,25 @@ import { Link } from "react-router-dom";
 
 const DrinkCard = ({ image, title }) => {
 	return (
-		<div>
-			<li className={css.card}>
-				<div className={css.imgWrap}>
-					<img
-						className={css.image}
-						src={image}
-						alt={title}
-						onError={e => {
-							e.target.src = DrinkPlaceholder;
-						}}
-					/>
-				</div>
-				<div className={css.ingredientsWrapper}>
-					<h3 className={css.drinkName}>{title}</h3>
+		<li className={css.card}>
+			<div className={css.imgWrap}>
+				<img
+					className={css.image}
+					src={image}
+					alt={title}
+					onError={e => {
+						e.target.src = DrinkPlaceholder;
+					}}
+				/>
+			</div>
+			<div className={css.ingredientsWrapper}>
+				<h3 className={css.drinkName}>{title}</h3>
 
-					<Link to="recipe/:recipeId">
-						<span className={css.ingredients}>Ingredients</span>
-					</Link>
-				</div>
-			</li>
-		</div>
+				<Link to="recipe/:recipeId">
+					<span className={css.ingredients}>Ingredients</span>
+				</Link>
+			</div>
+		</li>
 	);
 };
 
