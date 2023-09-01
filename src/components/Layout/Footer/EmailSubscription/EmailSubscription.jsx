47d4b547687d502fe9b9aab218bd66eafef13fa7
ansachuk <ansachuk@icloud.com>
2023-09-01 19:45:00 +0300

    fix lint

--- src/components/Layout/Footer/EmailSubscription/EmailSubscription.jsx
+++ src/components/Layout/Footer/EmailSubscription/EmailSubscription.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Formik, Form, Field } from "formik";
 import { subscribe } from "../../../../redux/operations/authOperations";
