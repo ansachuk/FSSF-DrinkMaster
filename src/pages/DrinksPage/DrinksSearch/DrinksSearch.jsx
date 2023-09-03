5c879d2266a51a15e6d79c76bb73c001424fb5d9
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-09-03 14:13:08 +0300

    update

--- src/pages/DrinksPage/DrinksSearch/DrinksSearch.jsx
+++ src/pages/DrinksPage/DrinksSearch/DrinksSearch.jsx
@@ -1,126 +1,129 @@
-// DrinkSearch.jsx
-import { useState } from "react";
-import PropTypes from "prop-types";
-import css from "./DrinksSearch.module.scss";
-import icons from "../../../images/icons.svg";
-import Select from "react-select";
-import { stylesDrink } from "./drinkSelectStyle";
-import { search } from "../../../redux/operations/recipiesOperations";
-import { useDispatch } from "react-redux";
 
-const desktopLimit = 9;
-const tabletLimit = 8;
 
-// const DrinksSearch = ({ categoriesList, ingredientsList, page }) => {
-// 	const [chosenCategory, setChosenCategory] = useState(null);
-// 	const [chosenIngredient, setChosenIngredient] = useState(null);
 
-// 	const [searchText, searchWord] = useState("");
+// // DrinkSearch.jsx
+// import { useState } from "react";
+// import PropTypes from "prop-types";
+// import css from "./DrinksSearch.module.scss";
+// import icons from "../../../images/icons.svg";
+// import Select from "react-select";
+// import { stylesDrink } from "./drinkSelectStyle";
+// import { search } from "../../../redux/operations/recipiesOperations";
+// import { useDispatch } from "react-redux";
 
-// 	const dispatch = useDispatch();
+// const desktopLimit = 9;
+// const tabletLimit = 8;
 
-const DrinksSearch = ({
-    categoriesList,
-    ingredientsList,
-    selectedCategory,
-    setSelectedCategory,
-    selectedIngredient,
-    setSelectedIngredient,
-    searchText,
-    setSearchText,
-    onSearch,
-}) => {
-    const dispatch = useDispatch();
+// // const DrinksSearch = ({ categoriesList, ingredientsList, page }) => {
+// // 	const [chosenCategory, setChosenCategory] = useState(null);
+// // 	const [chosenIngredient, setChosenIngredient] = useState(null);
+
+// // 	const [searchText, searchWord] = useState("");
+
+// // 	const dispatch = useDispatch();
+
+// const DrinksSearch = ({
+//     categoriesList,
+//     ingredientsList,
+//     selectedCategory,
+//     setSelectedCategory,
+//     selectedIngredient,
+//     setSelectedIngredient,
+//     searchText,
+//     setSearchText,
+//     onSearch,
+// }) => {
+//     const dispatch = useDispatch();
 
 	
- const isDesktop = typeof window !== "undefined" && window.innerWidth >= 1440;
-    const limit = isDesktop ? desktopLimit : tabletLimit;
+//  const isDesktop = typeof window !== "undefined" && window.innerWidth >= 1440;
+//     const limit = isDesktop ? desktopLimit : tabletLimit;
 
-    const handleSearchClick = (e) => {
-        e.preventDefault();
-        onSearch();
-    };
+//     const handleSearchClick = (e) => {
+//         e.preventDefault();
+//         onSearch();
+//     };
  
 
-	return (
-		<div className={css.wrapper}>
-			<form
-				onSubmit={handleSearchClick}
-				className={css.form}
-			>
-				<input
-					type="text"
-					className={css.input}
-					placeholder="Enter the text"
-					value={searchText}
-					onChange={e => searchWord(e.target.value)}
-				/>
-				<button
-					type="submit"
-					className={css.submit}
-					// onClick={e => {
-					// 	handleSearchClick(e);
-					// }}
-				>
-					<div className={css.hoverWrapper}></div>
-					<svg
-						className={css.iconDgink}
-						width="18"
-						height="18"
-					>
-						<use href={icons + "#search"}></use>
-					</svg>
-				</button>
-			</form>
-			<Select
-				options={categoriesList.map(category => {
-					console.log(category);
-					return { label: category };
-				})}
-				placeholder="All categories"
-				value={chosenCategory}
-				// onChange={e => setChosenCategory(e.target)}
-				onChange={(selectedOption) => setSelectedCategory(selectedOption)}
-				styles={stylesDrink}
-				unstyled
-				required
-			/>
-			<Select
-				options={ingredientsList.map(ingredient => {
-					return { value: ingredient._id, label: ingredient.title };
-				})}
-				placeholder="Ingredients"
-				value={chosenIngredient}
-				// onChange={selectedOption => setChosenIngredient(selectedOption)}
-				onChange={(selectedOption) => setSelectedIngredient(selectedOption)}
-				styles={stylesDrink}
-				unstyled
-				required
-			/>
-		</div>
-	);
-};
-
-// DrinksSearch.propTypes = {
-// 	onSearch: PropTypes.func.isRequired,
-// 	categoriesList: PropTypes.array.isRequired,
-// 	ingredientsList: PropTypes.array.isRequired,
-// 	// page: PropTypes.number.isRequired,
+// 	return (
+// 		<div className={css.wrapper}>
+// 			<form
+// 				onSubmit={handleSearchClick}
+// 				className={css.form}
+// 			>
+// 				<input
+// 					type="text"
+// 					className={css.input}
+// 					placeholder="Enter the text"
+// 					value={searchText}
+// 					onChange={e => searchWord(e.target.value)}
+// 				/>
+// 				<button
+// 					type="submit"
+// 					className={css.submit}
+// 					// onClick={e => {
+// 					// 	handleSearchClick(e);
+// 					// }}
+// 				>
+// 					<div className={css.hoverWrapper}></div>
+// 					<svg
+// 						className={css.iconDgink}
+// 						width="18"
+// 						height="18"
+// 					>
+// 						<use href={icons + "#search"}></use>
+// 					</svg>
+// 				</button>
+// 			</form>
+// 			<Select
+// 				options={categoriesList.map(category => {
+// 					console.log(category);
+// 					return { label: category };
+// 				})}
+// 				placeholder="All categories"
+// 				value={chosenCategory}
+// 				// onChange={e => setChosenCategory(e.target)}
+// 				onChange={(selectedOption) => setSelectedCategory(selectedOption)}
+// 				styles={stylesDrink}
+// 				unstyled
+// 				required
+// 			/>
+// 			<Select
+// 				options={ingredientsList.map(ingredient => {
+// 					return { value: ingredient._id, label: ingredient.title };
+// 				})}
+// 				placeholder="Ingredients"
+// 				value={chosenIngredient}
+// 				// onChange={selectedOption => setChosenIngredient(selectedOption)}
+// 				onChange={(selectedOption) => setSelectedIngredient(selectedOption)}
+// 				styles={stylesDrink}
+// 				unstyled
+// 				required
+// 			/>
+// 		</div>
+// 	);
 // };
 
-DrinksSearch.propTypes = {
-    onSearch: PropTypes.func.isRequired,
-    categoriesList: PropTypes.array.isRequired,
-    ingredientsList: PropTypes.array.isRequired,
-    selectedCategory: PropTypes.object,
-    setSelectedCategory: PropTypes.func,
-    selectedIngredient: PropTypes.object,
-    setSelectedIngredient: PropTypes.func,
-    searchText: PropTypes.string,
-    setSearchText: PropTypes.func,
-};
+// // DrinksSearch.propTypes = {
+// // 	onSearch: PropTypes.func.isRequired,
+// // 	categoriesList: PropTypes.array.isRequired,
+// // 	ingredientsList: PropTypes.array.isRequired,
+// // 	// page: PropTypes.number.isRequired,
+// // };
 
-export default DrinksSearch;
+// DrinksSearch.propTypes = {
+//     onSearch: PropTypes.func.isRequired,
+//     categoriesList: PropTypes.array.isRequired,
+//     ingredientsList: PropTypes.array.isRequired,
+//     selectedCategory: PropTypes.object,
+//     setSelectedCategory: PropTypes.func,
+//     selectedIngredient: PropTypes.object,
+//     setSelectedIngredient: PropTypes.func,
+//     searchText: PropTypes.string,
+//     setSearchText: PropTypes.func,
+// };
+
+// export default DrinksSearch;
 
 
 // ИСХОДНЫЙ
