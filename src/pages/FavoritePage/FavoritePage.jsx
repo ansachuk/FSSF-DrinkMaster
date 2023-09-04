3055fba5e934d373264c2a3358cf12f62a88a1d3
Heorhii <gerythepatient@gmail.com>
2023-09-04 22:05:36 +0200

    Fixed favorites

--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -1,66 +1,53 @@
 import { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import MainTitle from "../../components/MainTitle/MainTitle";
 import RecipesList from "../../components/RecipesList/RecipesList";
 import Paginator from "../../components/Paginator/Paginator";
 import { favorite } from "../../redux/operations/recipiesOperations.js";
-// import { selectIsLoggedIn } from "../../redux/selectors/authSelectors";
+import { selectIsLoggedIn } from "../../redux/selectors/authSelectors";
 
 import css from "./FavoritePage.module.scss";
 
 export default function FavoritePage() {
-	const dispatch = useDispatch();
-	const [totalHits, setTotalHits] = useState(30);
-	// const totalHits = favorite.result ? favorite.result.length : 0;
-	// const isLoggedIn = useSelector(selectIsLoggedIn);
-	const [page, setPage] = useState(1);
-	const [limit, setLimit] = useState(9);
+    const dispatch = useDispatch();
+    const [totalHits, setTotalHits] = useState(30);
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+    const [page, setPage] = useState(1);
+    const [limit, setLimit] = useState(9);
 
-	useEffect(() => {
-		dispatch(
-			favorite({
-				page: 1,
-				limit: 9,
-			}),
-		);
-	}, [dispatch, page, limit]);
+    useEffect(() => {
+        if (isLoggedIn) {
+            dispatch(
+                favorite({
+                    page: 1,
+                    limit: 9,
+                }),
+            );
+        }
+    }, [dispatch, isLoggedIn]);
 
-	// useEffect(() => {
-	// 	async function fetchFavorite() {
-	// 		const res = await dispatch(favorite());
-	// 		return res;
-	// 	}
-	// 	function loadPage() {
-	// 		if (isLoggedIn) {
-	// 			fetchFavorite();
-	// 		}
-	// 	}
-	// 	loadPage();
-	// }, [dispatch, isLoggedIn]);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
-
-	return (
-		<>
-			<div className={css.section}>
-				<MainTitle title="Favorites">
-					<RecipesList
-						page={page}
-						limit={limit}
-					/>
-					<Paginator
-						page={page}
-						setPage={setPage}
-						limit={limit}
-						setLimit={setLimit}
-						totalHits={totalHits}
-						setTotalHits={setTotalHits}
-					/>
-				</MainTitle>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className={css.section}>
+                <MainTitle title="Favorites">
+                    <RecipesList
+                        page={page}
+                        limit={limit}
+                    />
+                    <Paginator
+                        page={page}
+                        setPage={setPage}
+                        limit={limit}
+                        setLimit={setLimit}
+                        totalHits={totalHits}
+                        setTotalHits={setTotalHits}
+                    />
+                </MainTitle>
+            </div>
+        </>
+    );
 }
--- src/pages/MyRecipesPage/MyRecipesPage.jsx
+++ src/pages/MyRecipesPage/MyRecipesPage.jsx
@@ -1,27 +1,34 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import MainTitle from "../../components/MainTitle/MainTitle";
 import MyRecipeList from "../../pages/MyRecipesPage/MyRecipeList/MyRecipeList";
 import { own } from "../../redux/operations/recipiesOperations.js";
 import css from "./MyRecipesPage.module.scss";
+// import { selectOwn } from "../../redux/selectors/recipieSelectors";
+import { selectIsLoggedIn } from "../../redux/selectors/authSelectors";
 
 export default function MyRecipesPage() {
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(own());
-	}, [dispatch]);
+    const dispatch = useDispatch();
+    // const myOwnRecipes = useSelector(selectOwn);
+    const isLoggedIn = useSelector(selectIsLoggedIn);
 
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
+    useEffect(() => {
+        if (isLoggedIn) {
+            dispatch(own());
+        }
+    }, [dispatch, isLoggedIn]);
 
-	return (
-		<>
-			<div className={css.section}>
-				<MainTitle title="My recipes">
-					<MyRecipeList />
-				</MainTitle>
-			</div>
-		</>
-	);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    return (
+        <>
+            <div className={css.section}>
+                <MainTitle title="My recipes">
+                    <MyRecipeList />
+                </MainTitle>
+            </div>
+        </>
+    );
 }
