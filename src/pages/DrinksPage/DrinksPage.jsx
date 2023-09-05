b8d8811c6d00b4e2efd81dc8b88f2ebb9d6bf55b
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-09-05 23:24:43 +0300

    update

--- src/pages/DrinksPage/DrinksPage.jsx
+++ src/pages/DrinksPage/DrinksPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectSearchResults } from "../../redux/selectors/recipieSelectors";
 import css from "./DrinksPage.module.scss";
@@ -8,9 +8,9 @@ import DrinksList from "./DrinksList/DrinksList";
 import { allIngredients } from "../../redux/operations/recipiesOperations";
 
 const DrinksPage = () => {
-	const [isRender, setIsRender] = useState(false);
 	const searchList = useSelector(selectSearchResults);
-	const [page, setPage] = useState(1);
+	const [isRender, setIsRender] = useState(false);
+	const [page] = useState(1);
 
 	const dispatch = useDispatch();
 
@@ -20,7 +20,7 @@ const DrinksPage = () => {
 		} else {
 			setIsRender(false);
 		}
-	});
+	}, [searchList]);
 
 	useEffect(() => {
 		dispatch(allIngredients());
@@ -37,3 +37,43 @@ const DrinksPage = () => {
 };
 
 export default DrinksPage;
+
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { selectSearchResults } from "../../redux/selectors/recipieSelectors";
+// import css from "./DrinksPage.module.scss";
+// import MainTitle from "../../components/MainTitle/MainTitle";
+// import DrinksSearch from "./DrinksSearch/DrinksSearch";
+// import DrinksList from "./DrinksList/DrinksList";
+// import { allIngredients } from "../../redux/operations/recipiesOperations";
+
+// const DrinksPage = () => {
+// 	const [setIsRender] = useState(false);
+// 	const searchList = useSelector(selectSearchResults);
+// 	const [page] = useState(1);
+
+// 	const dispatch = useDispatch();
+
+// 	useEffect(() => {
+// 		if (searchList.length !== 0) {
+// 			setIsRender(true);
+// 		} else {
+// 			setIsRender(false);
+// 		}
+// 	});
+
+// 	useEffect(() => {
+// 		dispatch(allIngredients());
+// 	}, []);
+
+// 	return (
+// 		<MainTitle title={"Drinks"}>
+// 			<section className={css.section}>
+// 				<DrinksSearch page={page} />
+// 				<DrinksList results={searchList} />
+// 			</section>
+// 		</MainTitle>
+// 	);
+// };
+
+// export default DrinksPage;
