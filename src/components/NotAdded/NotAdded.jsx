97d01051babaefa7384d9123fd10ac33e1f72980
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-09-06 15:34:30 +0300

    add picture

--- src/components/NotAdded/NotAdded.jsx
+++ src/components/NotAdded/NotAdded.jsx
@@ -1,13 +1,28 @@
 import PropTypes from "prop-types";
 
 import Container from "../Container/Container";
-import DrinkDefault from "../../images/static/main/main.jpg";
+import DrinkDefault from "../../images/static/main/Drink/main@1x.jpg";
 import css from "./NotAdded.module.scss";
 
 export default function NotAdded({ text }) {
 	return (
 		<Container>
 			<div className={css.wrapDiv}>
+				{/* <picture>
+					<source
+						srcset="src/images/static/main/Drink/main@1x.webp 1x, src/images/static/main/Drink//main@2x.webp 2x"
+						type="image/webp"
+					/>
+					<source
+						srcset="src/images/static/main/Drink/main@1x.jpg 1x, src/images/static/main/Drink/main@2x.jpg 2x"
+						type="image/jpeg"
+					/>
+					<img
+						className={css.notFoundImg}
+						src={DrinkDefault}
+						alt="No recipe"
+					/>
+				</picture> */}
 				<img
 					className={css.notFoundImg}
 					src={DrinkDefault}
--- src/pages/NotFoundPage/NotFound/NotFound.jsx
+++ src/pages/NotFoundPage/NotFound/NotFound.jsx
@@ -1,14 +1,30 @@
-import DrinkDefault from "../../../images/static/main/main.jpg";
+import DrinkDefault from "../../../images/static/main/Drink/main@1x.jpg";
 import css from "../NotFound/NotFound.module.scss";
 
 export default function NotFoundPage() {
 	return (
 		<div className={css.imgBox}>
+			{/* <picture>
+				<source
+					srcset="../../../images/static/main/Drink/main@1x.webp 1x, ../../../images/static/main/Drink//main@2x.webp 2x"
+					type="image/webp"
+				/>
+				<source
+					srcset="../../../images/static/main/Drink/main@1x.jpg 1x, ../../../images/static/main/Drink/main@2x.jpg 2x"
+					type="image/jpeg"
+				/>
+				<img
+					className={css.notFoundImg}
+					src={DrinkDefault}
+					alt="Not Found"
+				/>
+			</picture> */}
 			<img
 				className={css.notFoundImg}
 				src={DrinkDefault}
 				alt="Not Found"
 			/>
+
 			<p className={css.notFoundText}>
 				<span>4</span> <span>4</span>
 			</p>
