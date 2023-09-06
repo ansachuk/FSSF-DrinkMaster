07b4b63074d25017cbcb94eaca537f66dc32ede2
Mikhailo Salko <slipknotmisha@gmail.com>
2023-09-07 01:01:25 +0300

    remove import useNavigate

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy, useEffect } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import { refresh } from "../../redux/operations/authOperations";
